7cd1e60eabb65f815ebb266bbd0c981f43aed71e
Georgiy Frolov <gfrolov@sensics.com>
2016-03-14 19:54:50 -0400

    add main plugin source

---
+++ com_osvr_Vive.cpp
@@ -0,0 +1,102 @@
+/** @date 2015
+
+    @author
+    Sensics, Inc.
+    <http://sensics.com/osvr>
+*/
+
+// Copyright 2015 Vuzix Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Internal Includes
+#include <osvr/PluginKit/PluginKit.h>
+#include <osvr/PluginKit/TrackerInterfaceC.h>
+#include <ViveClientDriver.h>
+#include <ViveDeviceDriver.h>
+#include <ViveServerDriver.h>
+
+// Generated JSON header file
+#include "com_osvr_Vive_json.h"
+
+// Library/third-party includes
+#include <math.h>
+#include <openvr_driver.h>
+
+// Standard includes
+#include <iostream>
+#include <memory>
+#include <vector>
+
+using namespace vr;
+
+// Anonymous namespace to avoid symbol collision
+namespace {
+
+class ViveDevice {
+  public:
+    ViveDevice(OSVR_PluginRegContext ctx) {
+        /// Create the initialization options
+        OSVR_DeviceInitOptions opts = osvrDeviceCreateInitOptions(ctx);
+
+        osvrDeviceTrackerConfigure(opts, &m_tracker);
+
+        /// Create the sync device token with the options
+        m_dev.initSync(ctx, "Vive", opts);
+
+        /// Send JSON descriptor
+        m_dev.sendJsonDescriptor(com_osvr_Vive_json);
+
+        /// Register update callback
+        m_dev.registerUpdateCallback(this);
+    }
+
+    OSVR_ReturnCode update() { return OSVR_RETURN_SUCCESS; }
+
+  private:
+    osvr::pluginkit::DeviceToken m_dev;
+    OSVR_TrackerDeviceInterface m_tracker;
+};
+
+class HardwareDetection {
+  public:
+    HardwareDetection() {}
+    OSVR_ReturnCode operator()(OSVR_PluginRegContext ctx) {
+
+        std::cout << "PLUGIN: Got a hardware detection request" << std::endl;
+
+        if (m_found) {
+            return OSVR_RETURN_SUCCESS;
+        }
+
+        else {
+            return OSVR_RETURN_FAILURE;
+        }
+        return OSVR_RETURN_SUCCESS;
+    }
+
+  private:
+    bool m_found;
+    ViveServerDriver m_serverDriver;
+    ViveClientDriver m_clientDriver;
+};
+} // namespace
+
+OSVR_PLUGIN(com_osvr_Vive) {
+    osvr::pluginkit::PluginContext context(ctx);
+
+    /// Register a detection callback function object.
+    context.registerHardwareDetectCallback(new HardwareDetection());
+
+    return OSVR_RETURN_SUCCESS;
+}
